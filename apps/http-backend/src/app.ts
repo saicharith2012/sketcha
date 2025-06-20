5215b9c8176fafd6e142fc4c25011264485eba16
saicharith2012 <saicharithpalabindhela@gmail.com>
2025-06-21 00:41:46 +0530

    feat(dependencies): add cors package to enable cross origin resource sharing

--- apps/http-backend/src/app.ts
+++ apps/http-backend/src/app.ts
@@ -2,8 +2,15 @@ import express from "express";
 import userRouter from "./routes/user.routes";
 import roomRouter from "./routes/room.routes";
 import chatRouter from "./routes/chat.routes";
+import cors from "cors";
 
 const app: express.Express = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use(express.json());
 
