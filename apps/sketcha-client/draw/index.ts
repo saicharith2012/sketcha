cfb07ca900ae48f235f33c30f60c4c353b5a7145
saicharith2012 <saicharithpalabindhela@gmail.com>
2025-06-20 15:49:50 +0530

    feat(draw): implement shape type definitions and store the existing shapes in global variable to render everytime canvas clears.

--- apps/sketcha-client/draw/index.ts
+++ apps/sketcha-client/draw/index.ts
@@ -1,3 +1,20 @@
+type Shape =
+  | {
+      type: "rect";
+      x: number;
+      y: number;
+      width: number;
+      height: number;
+    }
+  | {
+      type: "circle";
+      centerX: number;
+      centerY: number;
+      radius: number;
+    };
+
+let existingShapes: Shape[] = [];
+
 export default function initDraw(canvas: HTMLCanvasElement) {
   const ctx = canvas.getContext("2d");
 
@@ -22,18 +39,41 @@ export default function initDraw(canvas: HTMLCanvasElement) {
 
   canvas.addEventListener("mouseup", (e) => {
     clicked = false;
+    const width = e.clientX - startX;
+    const height = e.clientY - startY;
+    existingShapes.push({
+      type: "rect",
+      x: startX,
+      y: startY,
+      width,
+      height,
+    });
   });
 
   canvas.addEventListener("mousemove", (e) => {
     if (clicked) {
       width = e.clientX - startX;
       height = e.clientY - startY;
-
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle = "rgba(0,0,0,1)";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      clearCanvas(canvas, ctx, existingShapes);
       ctx.strokeStyle = "rgba(255,255,255,1)";
       ctx.strokeRect(startX, startY, width, height);
+    }
+  });
+}
+
+function clearCanvas(
+  canvas: HTMLCanvasElement,
+  ctx: CanvasRenderingContext2D,
+  existingShapes: Shape[]
+) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "rgba(0,0,0,1)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  existingShapes.map((shape) => {
+    if (shape.type === "rect") {
+      ctx.strokeStyle = "rgba(255,255,255,1)";
+      ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
     }
   });
 }
